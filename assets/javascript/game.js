e01152d7a250f29148f12f0024245855aad23495
Plant Lady <codeplantsbylouie@gmail.com>
2019-02-01 11:45:41 -0500

    changes i made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,72 +6,59 @@ var computerChoices = ['a','b','c,','d','e','f','g','h','i','j','k','l','m','n',
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var userChoices = []; // empty array to push userChoices to
+var guessedLetters = [];
 
-// Create variables for wins, losses, guesses left, and guesses so far by connecting them to the id's in index.html (using .getElementByID)
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guessesLeft-text");
-var userChoicesText = document.getElementById("userChoices-Text");
-
-// Need to get user's key fxn; this will nest the rest of: var's for user guess(maybe comp's guess), if's, and updating text
+//CAPTURING THE PLAYER'S INPUT
+// when the user presses a key, it will run the following function...
 document.onkeyup = function(event) {
 
-  // variables for the User's guesses and and getting computer choices to be random
-  var userGuess = event.key; //taking the user guess
-  var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)]; //computer selects random letter
+  // taking the user's guess
+  var userGuess = event.key; 
 
-  // variable options, so i don't have to type indiv  ==== && and ||'s for each letter
-  var options =['a','b','c,','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s,','t','u','v','w','x','y','z'];
-  while (guessesLeft > 0 && guessesLeft < 10){
+  //GENERATING THE COMPUTER'S LETTER
+  //generates random number (decimal from 0 to 1) which is multiplied by the length of computerChoices array
+  //and then rounded down to the nearest whole number by the Math.floor method
+  var computerLetter = computerChoices[Math.floor(Math.random()*computerChoices.length)]; 
 
-    if (options.indexOf(userGuess) > -1) { //maybe this isn't right.........
-    //if userGuess = computerGuess then wins will go up +1, guessesLeft will stay the same, and userChoices will log the letter user chose
-    if (userGuess === computerGuess) {
+  //HANDLING CORRECT GUESSES
+    if (userGuess === computerLetter) {
       wins++;
+      guessedLetters = [];
+      guessesLeft = 9;
     }
-    //if userGuess does not equal computerGuess then guessesLeft will go down -1, userChoices will log the letter user chose
-    if (userGuess != computerGuess) {
+    //HANDLING INCORRECT GUESSES
+    //test if user guess Does Not Equal computerLetter and decriments attempts by 1
+    if (userGuess != computerLetter) {
       guessesLeft--;
-      userChoices.push(userGuess);
+      guessedLetters.push(userGuess);
     }
-    //if guessesLeft = 0 then guessesLeft will go back to 9, losses will go up +1, and userChoices will clear out 
+    //when remaining attempts equals 0, losses is incrememented by 1; attempts is reset to 9, and guessedLetters array is cleared
     if (guessesLeft === 0) {
-      // guessesLeft = 9;
+      guessesLeft = 9;
       losses++;
-      // userChoices.length = 0;
+      guessedLetters = [];
     }
+    //HANDLING INCORRECT GUESSES - OUTPUT
+    //this 'if' prevents a letter selected a 2nd time from being written to the guessedLetters array again; still counts as a guess
+    if (guessedLetters.indexOf(userGuess) >= 0) {
 
-  }
-  winsText.textContent = "Wins: " + wins;
-  lossesText.textContent = "Losses: " + losses;
-  guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-  userChoicesText.textContent = "Your guesses so far: " + userChoices;
-
+    } else {
+      //this pushes the players incorrect guess to the guessedLetters array and writes it to the HTMl
+      guessedLetters.push(userGuess);
+      document.getElementById('guessedLetters') = guessedLetters;
+      console.log(guessedLetters);
+    }
+    //OUTPUT TO HTML
+    //Create variables for wins, losses, guesses left, and guesses so far by connecting them to the id's in index.html (using .getElementByID)
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+    var guessesLeftText = document.getElementById("guessesLeft-text");
+    var guessedLettersText = document.getElementById("guessedLetters-Text");
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+    guessedLettersText.textContent = "Your guesses so far: " + guessedLetters.push(', ');
+  
 }
 
-  // // this if holds...
-  // if (options.indexOf(userGuess) > -1) { //maybe this isn't right.........
-  //   //if userGuess = computerGuess then wins will go up +1, guessesLeft will stay the same, and userChoices will log the letter user chose
-  //   if (userGuess === computerGuess) {
-  //     wins++;
-  //   }
-  //   //if userGuess does not equal computerGuess then guessesLeft will go down -1, userChoices will log the letter user chose
-  //   if (userGuess != computerGuess) {
-  //     guessesLeft--;
-  //     userChoices.push(userGuess);
-  //   }
-  //   //if guessesLeft = 0 then guessesLeft will go back to 9, losses will go up +1, and userChoices will clear out 
-  //   if (guessesLeft === 0) {
-  //     // guessesLeft = 9;
-  //     losses++;
-  //     // userChoices.length = 0;
-  //   }
- 
-  //   winsText.textContent = "Wins: " + wins;
-  //   lossesText.textContent = "Losses: " + losses;
-  //   guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-  //   userChoicesText.textContent = "Your guesses so far: " + userChoices;
 
-  // }
-}
