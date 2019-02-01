488ee0034ef857af33f79281ff0c5af25724ea10
Plant Lady <codeplantsbylouie@gmail.com>
2019-02-01 08:55:23 -0500

    changes i made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,53 +2,76 @@
 // Create a variable 'computerChoices'; will hold alphabet in the array
 var computerChoices = ['a','b','c,','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s,','t','u','v','w','x','y','z'];
 
-// Set variables equal to 0 and start guessleft at 10(or any number)
+// Set variables equal to 0 and start guessesleft at 9
 var wins = 0;
 var losses = 0;
-var guessLeft = 10;
-var userGuesses = [];
+var guessesLeft = 9;
+var userChoices = []; // empty array to push userChoices to
 
 // Create variables for wins, losses, guesses left, and guesses so far by connecting them to the id's in index.html (using .getElementByID)
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var guessLeftText = document.getElementById("guessLeft-text");
-var userGuessesText = document.getElementById("userGuesses-Text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var userChoicesText = document.getElementById("userChoices-Text");
 
 // Need to get user's key fxn; this will nest the rest of: var's for user guess(maybe comp's guess), if's, and updating text
 document.onkeyup = function(event) {
 
   // variables for the User's guesses and and getting computer choices to be random
-  var userGuesses = event.key;
-  var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+  var userGuess = event.key; //taking the user guess
+  var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)]; //computer selects random letter
 
-  if ((userGuesses === 'a') || (userGuesses === 'b') || (userGuesses === 'c') || (userGuesses === 'd') ||
-  (userGuesses === 'e') || (userGuesses === 'f') || (userGuesses === 'g') || (userGuesses === 'h') || (userGuesses === 'i') ||
-  (userGuesses === 'j') || (userGuesses === 'k') || (userGuesses === 'l') || (userGuesses === 'm') || (userGuesses === 'n') ||
-  (userGuesses === 'o') || (userGuesses === 'p') || (userGuesses === 'q') || (userGuesses === 'r') || (userGuesses === 's') ||
-  (userGuesses === 't') || (userGuesses === 'u') || (userGuesses === 'v') || (userGuesses === 'w') || (userGuesses === 'x') ||
-  (userGuesses === 'y') || (userGuesses === 'z')) {
-    if ((userGuesses === 'a') && (computerGuess === 'a') || (userGuesses === 'b') && (computerGuess === 'b') || 
-    (userGuesses === 'c') && (computerGuess === 'c') || (userGuesses === 'd') && (computerGuess === 'd') ||
-    (userGuesses === 'e') && (computerGuess === 'e') || (userGuesses === 'f') && (computerGuess === 'f') ||
-    (userGuesses === 'g') && (computerGuess === 'g') || (userGuesses === 'h') && (computerGuess === 'h') ||
-    (userGuesses === 'i') && (computerGuess === 'i') || (userGuesses === 'j') && (computerGuess === 'j') ||
-    (userGuesses === 'k') && (computerGuess === 'k') || (userGuesses === 'l') && (computerGuess === 'l') ||
-    (userGuesses === 'm') && (computerGuess === 'm') || (userGuesses === 'n') && (computerGuess === 'n') ||
-    (userGuesses === 'o') && (computerGuess === 'o') || (userGuesses === 'p') && (computerGuess === 'p') ||
-    (userGuesses === 'q') && (computerGuess === 'q') || (userGuesses === 'r') && (computerGuess === 'r') ||
-    (userGuesses === 's') && (computerGuess === 's') || (userGuesses === 't') && (computerGuess === 't') ||
-    (userGuesses === 'u') && (computerGuess === 'u') || (userGuesses === 'v') && (computerGuess === 'v') ||
-    (userGuesses === 'w') && (computerGuess === 'w') || (userGuesses === 'x') && (computerGuess === 'x') ||
-    (userGuesses === 'y') && (computerGuess === 'y') || (userGuesses === 'z') && (computerGuess === 'z'));
-    } else {
-    losses++;
-    guessLeft--;
+  // variable options, so i don't have to type indiv  ==== && and ||'s for each letter
+  var options =['a','b','c,','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s,','t','u','v','w','x','y','z'];
+  while (guessesLeft > 0 && guessesLeft < 10){
+
+    if (options.indexOf(userGuess) > -1) { //maybe this isn't right.........
+    //if userGuess = computerGuess then wins will go up +1, guessesLeft will stay the same, and userChoices will log the letter user chose
+    if (userGuess === computerGuess) {
+      wins++;
     }
- 
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    guessLeftText.textContent = "Guesses Left: " + guessLeft;
-    userGuessesText.textContent = "Your guesses so far: " + userGuesses;
+    //if userGuess does not equal computerGuess then guessesLeft will go down -1, userChoices will log the letter user chose
+    if (userGuess != computerGuess) {
+      guessesLeft--;
+      userChoices.push(userGuess);
+    }
+    //if guessesLeft = 0 then guessesLeft will go back to 9, losses will go up +1, and userChoices will clear out 
+    if (guessesLeft === 0) {
+      // guessesLeft = 9;
+      losses++;
+      // userChoices.length = 0;
+    }
 
   }
+  winsText.textContent = "Wins: " + wins;
+  lossesText.textContent = "Losses: " + losses;
+  guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+  userChoicesText.textContent = "Your guesses so far: " + userChoices;
 
+}
+
+  // // this if holds...
+  // if (options.indexOf(userGuess) > -1) { //maybe this isn't right.........
+  //   //if userGuess = computerGuess then wins will go up +1, guessesLeft will stay the same, and userChoices will log the letter user chose
+  //   if (userGuess === computerGuess) {
+  //     wins++;
+  //   }
+  //   //if userGuess does not equal computerGuess then guessesLeft will go down -1, userChoices will log the letter user chose
+  //   if (userGuess != computerGuess) {
+  //     guessesLeft--;
+  //     userChoices.push(userGuess);
+  //   }
+  //   //if guessesLeft = 0 then guessesLeft will go back to 9, losses will go up +1, and userChoices will clear out 
+  //   if (guessesLeft === 0) {
+  //     // guessesLeft = 9;
+  //     losses++;
+  //     // userChoices.length = 0;
+  //   }
+ 
+  //   winsText.textContent = "Wins: " + wins;
+  //   lossesText.textContent = "Losses: " + losses;
+  //   guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+  //   userChoicesText.textContent = "Your guesses so far: " + userChoices;
+
+  // }
+}
